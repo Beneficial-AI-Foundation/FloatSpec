fba9cf0693232df80c7a31fe9a2a1e145a592f57
Serhii Khoma <sergey@nordicresults.com>
2026-05-17 00:23:49 +0700

    feat: lakefile.lean -> make mathlib a reservoir dependency so now can use `lake exe cache get` to prefetch mathlibs olean

--- lakefile.lean
+++ lakefile.lean
@@ -29,9 +29,8 @@ package FloatSpec where
 
 /-! Dependencies -/
 
-require cslib from git "https://github.com/leanprover/cslib" @ "v4.30.0-rc2"
-
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.30.0-rc2"
+require "leanprover" / "cslib" -- @ git "v4.30.0-rc2"
+require "leanprover-community" / "mathlib" -- @ git "v4.30.0-rc2"
 
 /-- Linters for FloatSpec (prefer grind over omega, etc).
     Stdlib only, provides linter.preferGrind option.
